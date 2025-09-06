ee090a6ebc3150227c182cfa67e6a25e0616eae7
mo3ito <mostafa.tar90@gmail.com>
2025-09-06 18:22:24 +0330

    edit on next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
   images: {
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    domains: ["localhost", "anit-test.ir"],
+    domains: ["localhost", "dekamond-umber.vercel.app"],
     minimumCacheTTL: 604800,
     remotePatterns: [
       {
